a6b6e6df95c8e21ce925e1b88ddd8c977610137d
小权 <597141997@qq.com>
2020-06-20 17:36:42 +0800

    feat(all files): 新增url、send data、headers 处理

---
+++ example/base/app.ts
@@ -0,0 +1,100 @@
+import axios from "../../src/index"
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {bar: 'baz'}
+  }
+})
+
+const date = new Date();
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: "@:$, "
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?foo=bar',
+})
+
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: "bar11",
+    baz: null
+  }
+})
+
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: "bar",
+    bar: "baz"
+  }
+})
+
+axios({
+  method: "post",
+  url: "/base/post",
+  data: {
+    a: 1,
+    b: 2
+  }
+});
+
+const arr = new Int32Array([21, 31]);
+axios({
+  method: "post",
+  url: "/base/buffer",
+  data: arr
+});
+
+axios({
+  method: "post",
+  url: "/base/post",
+  headers: {
+    "content-type": 'application/json',
+    "Accept": "application/json,text/plain,*/*"
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+});
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: "post",
+  url: "/base/post",
+  data: searchParams
+});
+
+
--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,31 +1,48 @@
-import { isDate, isObject } from "./util";
-function encode(val: string): string{
-
+import { isDate, isPlainObject } from './util'
+function encode(val: string): string {
+  return encodeURIComponent(val)
+    .replace(/%40/g, '@')
+    .replace(/%3A/gi, ':')
+    .replace(/%24/g, '$')
+    .replace(/%2C/gi, ',')
+    .replace(/%20/g, '+')
+    .replace(/%5B/gi, '[')
+    .replace(/%5D/g, ']')
 }
-export function buildURL(url: string, params?: any): string{
-  if(!params){
-    return url;
+export function buildURL(url: string, params?: any): string {
+  if (!params) {
+    return url
   }
-  const parts: string[] = [];
-  Object.keys(params).forEach((key) => {
-    const val = params[key];
-    if(val === null || typeof val === 'undefined'){
+  const parts: string[] = []
+  Object.keys(params).forEach(key => {
+    const val = params[key]
+    if (val === null || typeof val === 'undefined') {
       return // 不执行往后代码
     }
-    let values = [];
-    if(Array.isArray(val)){
-      values = val;
+    let values = []
+    if (Array.isArray(val)) {
+      values = val
       key += '[]'
     } else {
-      values = [val];
+      values = [val]
     }
-    values.forEach((val) => {
-      if(isDate(val)){
+    values.forEach(val => {
+      if (isDate(val)) {
         val = val.toISOString()
-      }else if(isObject(val)){
-        val = JSON.stringify(val);
+      } else if (isPlainObject(val)) {
+        val = JSON.stringify(val)
       }
-      parts.push(`${key}=${val}`)
+      parts.push(`${encode(key)}=${encode(val)}`)
+      // parts.push(`${key}=${val}`)
     })
-  });
+  })
+  let serializedParams = parts.join('&')
+  if (serializedParams) {
+    const markIndex = url.indexOf('#')
+    if (markIndex !== -1) {
+      url = url.slice(0, markIndex)
+    }
+    url += (url.indexOf('?') !== -1 ? '&' : '?') + serializedParams
+  }
+  return url
 }
